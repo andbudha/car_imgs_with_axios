f94fc894c8f8d819f35f0a99d22d8b1a54afe702
andbudha <andreibartov@gmail.com>
2023-03-23 21:19:18 +0100

    axios applied

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+
+export const searchImages = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: 'Client-ID mU_x2mx8YuWouhYwI1mzOXxIIBIwMhwu6r9FWjqxxnM'
+        },
+        params: {
+            query: term
+        }
+    });
+
+    return response.data.results;
+}
